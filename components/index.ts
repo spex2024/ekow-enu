8835b333d86d42f4e20b951ee18bdcf2fdf8e988
spex2024 <dercolbags@gmail.com>
2024-12-12 12:12:08 +0000

    v1.0.0.0

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Badge } from "./badge"
-export { Button } from "./button"
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card"
-export { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs"
-
